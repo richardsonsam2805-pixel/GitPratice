fda94a97f851468f1edf75c19ad9acad99922aef
richardsonsam2805 <richardsonsam2805@gmail.com>
2026-05-29 22:17:19 +0530

    Method Add in Hooks Class

--- src/test/java/com/omrbranch/hooks/HooksClass.java
+++ src/test/java/com/omrbranch/hooks/HooksClass.java
@@ -1,5 +1,22 @@
 package com.omrbranch.hooks;
 
 public class HooksClass {
+	
+	public void m1() {
+		System.out.println("1");
+
+	}
+
+	public void m2() {
+		System.out.println("2");
+	}
+
+	public void m3() {
+		System.out.println("3");
+	}
+	
+	public void m4() {
+		System.out.println("4");
+	}
 
 }
